a752f318712ef7abe13837582c927570398a66c7
Kyllen02 <giovanni.roin@gmail.com>
2022-05-17 19:38:49 +0200

    Semplificata la funzione strncmp

--- test/src/strncmp.s
+++ test/src/strncmp.s
@@ -12,36 +12,38 @@ asm_strncmp:
         /* Salvataggio registri. */
         push %esi
         push %edi
+        push %ebx
 
     movl 8(%ebp), %esi      # Copia il primo parametro.
     movl 12(%ebp), %edi     # Copia il secondo parametro.
     movl 16(%ebp), %ecx     # Copia il terzo parametro.
 
     xorl %eax, %eax
+    xorl %ebx, %ebx
 
     strncmp_loop:
         movb (%esi), %al
-
-        test %al, %al           # Confronta il primo carattere con '\0'.
-        jz   strncmp_diff       # Se è uguale a '\0' esce dal ciclo.
-        cmpb %al, (%edi)        # Confronta il primo carattere con il secondo.
-        jne  strncmp_diff       # Se sono diversi esce dal ciclo.
-
         incl %esi
+        movb (%edi), %bl
         incl %edi
 
         test %ecx, %ecx         # Confronta il contatore con 0.
         jz   strncmp_diff       # Se è uguale a 0 esce dal ciclo.
         decl %ecx
+
+        test %al, %al           # Confronta il primo carattere con '\0'.
+        jz   strncmp_diff       # Se è uguale a '\0' esce dal ciclo.
+        cmpb %al, %bl           # Confronta il primo carattere con il secondo.
+        jne  strncmp_diff       # Se sono diversi esce dal ciclo.
+
         jmp  strncmp_loop
 
     strncmp_diff:
-        xorl %ecx, %ecx
-        movb (%edi), %cl
-        subl %ecx, %eax     # Restituisce la differenza tra i due caratteri puntati.
+        subl %ebx, %eax     # Restituisce la differenza tra i due caratteri puntati.
 
     strncmp_epilogue:
         /* Ripristino registri. */
+        pop %ebx
         pop %edi
         pop %esi
         /* Ripristino base ptr. */
